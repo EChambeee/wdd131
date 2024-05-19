2de25fbf789121827f4cd326de990a1e68dc0589
EChambeee <e.elizabeth.chamberlain@gmail.com>
2024-05-18 19:24:55 -0600

    cool pics

---
+++ W3/coolpics/coolpics.js
@@ -0,0 +1,32 @@
+// script.js
+function viewerTemplate(pic, alt) {
+    return `
+        <div class="viewer">
+            <button class="close-viewer">X</button>
+            <img src="${pic}" alt="${alt}">
+        </div>`;
+}
+
+function viewHandler(event) {
+    const clickedElement = event.target;
+
+    if (clickedElement.tagName === 'IMG' && clickedElement.classList.contains('thumbnail')) {
+        const imgSrcParts = clickedElement.src.split('-');
+        const imgSrc = imgSrcParts[0] + '-full.jpeg';
+        const imgAlt = clickedElement.alt;
+        const modalHTML = viewerTemplate(imgSrc, imgAlt);
+
+        document.body.insertAdjacentHTML('afterbegin', modalHTML);
+
+        document.querySelector('.close-viewer').addEventListener('click', closeViewer);
+    }
+}
+
+function closeViewer() {
+    const viewer = document.querySelector('.viewer');
+    if (viewer) {
+        viewer.remove();
+    }
+}
+
+document.querySelector('.gallery').addEventListener('click', viewHandler);
